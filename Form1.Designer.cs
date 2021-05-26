9a9e9f956e402561769a1802bfc3fddb33dc930f
Adrian <adrianalalusin@iskolarngbayan.pup.edu.ph>
2021-05-26 22:48:51 +0800

    implemented buttons that disables when clicked and shows which move.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -41,6 +41,7 @@ namespace TIC_TAC_TOE
             this.btnRestart = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
             this.txtScore = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btn1
@@ -50,8 +51,8 @@ namespace TIC_TAC_TOE
             this.btn1.Name = "btn1";
             this.btn1.Size = new System.Drawing.Size(100, 100);
             this.btn1.TabIndex = 0;
-            this.btn1.Text = "X";
             this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn_Click);
             // 
             // btn2
             // 
@@ -60,8 +61,8 @@ namespace TIC_TAC_TOE
             this.btn2.Name = "btn2";
             this.btn2.Size = new System.Drawing.Size(100, 100);
             this.btn2.TabIndex = 0;
-            this.btn2.Text = "X";
             this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn_Click);
             // 
             // btn3
             // 
@@ -70,8 +71,8 @@ namespace TIC_TAC_TOE
             this.btn3.Name = "btn3";
             this.btn3.Size = new System.Drawing.Size(100, 100);
             this.btn3.TabIndex = 0;
-            this.btn3.Text = "X";
             this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn_Click);
             // 
             // btn4
             // 
@@ -80,8 +81,8 @@ namespace TIC_TAC_TOE
             this.btn4.Name = "btn4";
             this.btn4.Size = new System.Drawing.Size(100, 100);
             this.btn4.TabIndex = 0;
-            this.btn4.Text = "X";
             this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn_Click);
             // 
             // btn5
             // 
@@ -90,8 +91,8 @@ namespace TIC_TAC_TOE
             this.btn5.Name = "btn5";
             this.btn5.Size = new System.Drawing.Size(100, 100);
             this.btn5.TabIndex = 0;
-            this.btn5.Text = "X";
             this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn_Click);
             // 
             // btn6
             // 
@@ -100,8 +101,8 @@ namespace TIC_TAC_TOE
             this.btn6.Name = "btn6";
             this.btn6.Size = new System.Drawing.Size(100, 100);
             this.btn6.TabIndex = 0;
-            this.btn6.Text = "X";
             this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn_Click);
             // 
             // btn7
             // 
@@ -110,8 +111,8 @@ namespace TIC_TAC_TOE
             this.btn7.Name = "btn7";
             this.btn7.Size = new System.Drawing.Size(100, 100);
             this.btn7.TabIndex = 0;
-            this.btn7.Text = "X";
             this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn_Click);
             // 
             // btn8
             // 
@@ -120,8 +121,8 @@ namespace TIC_TAC_TOE
             this.btn8.Name = "btn8";
             this.btn8.Size = new System.Drawing.Size(100, 100);
             this.btn8.TabIndex = 0;
-            this.btn8.Text = "X";
             this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn_Click);
             // 
             // btn9
             // 
@@ -130,8 +131,8 @@ namespace TIC_TAC_TOE
             this.btn9.Name = "btn9";
             this.btn9.Size = new System.Drawing.Size(100, 100);
             this.btn9.TabIndex = 0;
-            this.btn9.Text = "X";
             this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn_Click);
             // 
             // btnRestart
             // 
@@ -157,18 +158,30 @@ namespace TIC_TAC_TOE
             // txtScore
             // 
             this.txtScore.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtScore.Location = new System.Drawing.Point(356, 12);
+            this.txtScore.Location = new System.Drawing.Point(356, 59);
             this.txtScore.Name = "txtScore";
             this.txtScore.PlaceholderText = "SCORE";
             this.txtScore.ReadOnly = true;
             this.txtScore.Size = new System.Drawing.Size(206, 41);
             this.txtScore.TabIndex = 1;
             // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(356, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PlaceholderText = "SCORE";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(206, 41);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "TURN: X";
+            // 
             // TICTACTOE
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(588, 337);
+            this.ClientSize = new System.Drawing.Size(578, 337);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.txtScore);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnRestart);
@@ -203,6 +216,7 @@ namespace TIC_TAC_TOE
         private System.Windows.Forms.Button btnRestart;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,13 +12,39 @@ namespace TIC_TAC_TOE
 {
     public partial class TICTACTOE : Form
     {
+        bool turn = true;
+        List<string> moves = new List<string>();
+
         public TICTACTOE()
         {
             InitializeComponent();
         }
+        private void btn_Click(object sender, EventArgs e)
+        {
+            
+            Button btn = sender as Button;
 
+            if (turn)
+            {
+                btn.Text = "X";
+                btn.Enabled = false;
+                turn = false;
+                textBox2.Text = "TURN: O";
+                moves.Add("X");
+            }
+            else
+            {
+                btn.Text = "O";
+                btn.Enabled = false;
+                turn = true;
+                textBox2.Text = "TURN: X";
+                moves.Add("O");
+            }
+            
+        }
         private void btnExit_Click(object sender, EventArgs e)
         {
+            
             Application.Exit();
         }
     }
